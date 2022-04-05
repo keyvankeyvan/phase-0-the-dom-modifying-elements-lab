77d4cf4a968fc83431867345162c53560393d1e0
Keyvan Alonso <keyvanalonso@gmail.com>
2022-04-05 00:04:15 -0400

    commit tree c32623ba3009f284d7471d547efaf6cd59c76060

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// PART 1
+const element = document.getElementById('main')
+element.remove()
+
+// PART 2/3/4
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+newHeader.className = 'victory'
+newHeader.innerHTML = 'YOUR-NAME is the champion'
